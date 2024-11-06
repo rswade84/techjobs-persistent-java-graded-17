0b73c7b074f30e2a23af3b9dc2145acdfceb1b68
Renaldo <renaldowade@gmail.com>
2024-11-06 11:04:24 -0600

    Task #3 Complete

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,11 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity  // This is crucial - makes it a managed type
 public class Skill extends AbstractEntity {
+
+
+    // Task 4: Add a field named jobs
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     @NotNull
     @Size(max=500)
@@ -20,4 +29,9 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    // Getters and Setters
+    public List<Job> getJobs() { return jobs; }
+
+    public void setJobs(List<Job> jobs) { this.jobs = jobs; }
 }
