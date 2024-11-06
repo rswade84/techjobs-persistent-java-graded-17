ce84e0210f05f00d051501083efa81bc7444aa68
Renaldo <renaldowade@gmail.com>
2024-11-06 08:15:05 -0600

    Task #2 Complete

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("employers")
+@RequestMapping("/employers")
 public class EmployerController {
 
     // Task 2: Add a private field of EmployerRepository type called employerRepository
@@ -22,8 +22,9 @@ public class EmployerController {
 
     // Task 2: Add an index method that responds at /employers with a list of all employers
     // Uses employers/index template and adds list of all employers to the model
-    @GetMapping("")
-    public String index(Model model) {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("title", "All Employers");
         model.addAttribute("employers", employerRepository.findAll());
         return "employers/index";
     }
@@ -39,23 +40,21 @@ public class EmployerController {
     @PostMapping("add")
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                          Errors errors, Model model) {
-
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
             return "employers/add";
         }
-
         employerRepository.save(newEmployer);
-        return "redirect:";
+        return "redirect:/employers/";
     }
 
     // Task 2: Use employer object's id field to grab the correct information from employerRepository
     // Fixed Optional<Employer> to use employerRepository.findById()
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
-
-        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
-            Employer employer = optEmployer.get();
+            Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
             return "employers/view";
         } else {
