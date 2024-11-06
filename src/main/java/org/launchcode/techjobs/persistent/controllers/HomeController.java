ae93607c6e5926ee8dc12f64a1ea125c24c0c9fe
Renaldo <renaldowade@gmail.com>
2024-11-06 10:11:52 -0600

    Task #3 Complete

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,15 +1,15 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -17,37 +17,52 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    // Task 3: Add employerRepository with AutoWired annotation
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    // Autowire jobRepository as well
+    @Autowired
+    private JobRepository jobRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
-
         model.addAttribute("title", "MyJobs");
-
         return "index";
     }
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
-	model.addAttribute("title", "Add Job");
+        model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        // Task 3: Add employer data from employerRepository to the form template
+        model.addAttribute("employer", employerRepository.findAll());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId) {
+                                    Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
-	    model.addAttribute("title", "Add Job");
+            model.addAttribute("title", "Add Job");
             return "add";
         }
+
+        // Find employer by ID and set it on newJob
+        Employer employer = employerRepository.findById(employerId).orElse(null);
+        if (employer != null) {
+            newJob.setEmployer(employer);
+        }
+
+        // Save this new job
+        jobRepository.save(newJob);
 
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
-            return "view";
+        return "view";
     }
-
 }
